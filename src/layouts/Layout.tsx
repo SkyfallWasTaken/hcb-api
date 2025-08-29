92ccbac0832c5470672cd50f0969a10424264409
Mahad Kalam <mahadkalam1@proton.me>
2025-08-29 02:35:09 +0100

    Use Funnel Sans

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -4,8 +4,11 @@ export const Layout = ({ children, title = "HCB API Manager" }: { children: any,
       <title>{title}</title>
       <script src="https://cdn.tailwindcss.com"></script>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      <link href="https://fonts.googleapis.com/css2?family=Funnel+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet" />
     </head>
-    <body className="bg-[#16161f] text-[#a9b1d6]">
+    <body className="bg-[#16161f] text-[#a9b1d6]" style={{ fontFamily: 'Funnel Sans, sans-serif' }}>
       {children}
       <footer className="mt-8 mb-4 text-center text-sm text-[#7f8fa6]">
         made with <span className="text-[#f7768e]">&lt;3</span> by{' '}
