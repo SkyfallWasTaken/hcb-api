265343d5d36ea9b1108000aac452d5b582f9d968
Mahad Kalam <mahadkalam1@proton.me>
2025-09-22 15:39:33 +0100

    Fix builds, pls pls pls

--- Dockerfile
+++ Dockerfile
@@ -20,4 +20,4 @@ COPY --from=build /app/build ./build
 COPY --from=build /app/package.json ./package.json
 COPY --from=build /app/node_modules ./node_modules
 RUN ulimit -c unlimited
-ENTRYPOINT ["node", "build"]
+ENTRYPOINT ["bun", "build/server.js"]
--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -2,6 +2,7 @@ import { drizzle } from 'drizzle-orm/bun-sqlite';
 import { Database } from 'bun:sqlite';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
+import { migrate } from 'drizzle-orm/bun-sqlite/migrator';
 
 const dbUrl = env.DATABASE_URL || './data/database.db';
 
@@ -9,3 +10,5 @@ const sqlite = new Database(dbUrl);
 
 export const db = drizzle({client: sqlite, schema});
 export * from './schema';
+
+migrate(db, { migrationsFolder: './drizzle' });
