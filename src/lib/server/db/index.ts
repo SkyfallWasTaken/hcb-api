af57bb7d2ae00f1c3ff4d31ac3b555388186099b
Mahad Kalam <mahadkalam1@proton.me>
2025-09-22 15:32:56 +0100

    Use Bun SQLite

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,5 +1,5 @@
-import { drizzle } from 'drizzle-orm/better-sqlite3';
-import Database from 'better-sqlite3';
+import { drizzle } from 'drizzle-orm/bun-sqlite';
+import { Database } from 'bun:sqlite';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
@@ -7,5 +7,5 @@ const dbUrl = env.DATABASE_URL || './data/database.db';
 
 const sqlite = new Database(dbUrl);
 
-export const db = drizzle(sqlite, { schema });
+export const db = drizzle({client: sqlite, schema});
 export * from './schema';
