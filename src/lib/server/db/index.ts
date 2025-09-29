eeff1ea7e797fd66068cb14e771e9adcbe753ce9
Mahad Kalam <mahadkalam1@proton.me>
2025-09-29 11:25:26 +0100

    Log queries to the console

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -3,12 +3,19 @@ import { Database } from 'bun:sqlite';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 import { migrate } from 'drizzle-orm/bun-sqlite/migrator';
+import type { Logger } from 'drizzle-orm/logger';
+
+class ConsoleLogger implements Logger {
+	logQuery(query: string, params: unknown[]): void {
+		console.log({ query, params });
+	}
+}
 
 const dbUrl = env.DATABASE_URL || './data/database.db';
 
 const sqlite = new Database(dbUrl);
 
-export const db = drizzle({ client: sqlite, schema });
+export const db = drizzle({ client: sqlite, schema, logger: new ConsoleLogger() });
 export * from './schema';
 
 migrate(db, { migrationsFolder: './drizzle' });
